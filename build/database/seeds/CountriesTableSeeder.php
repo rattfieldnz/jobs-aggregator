797445ff6229d15a7f5f3b449e847ffc277ce8dc
Rob Attfield <emailme@robertattfield.com>
2015-12-10 20:52:40 +1300

    Updated project dependencies, modified Users table seeder, created initial Countries table seeder.

---
+++ build/database/seeds/CountriesTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class CountriesTableSeeder
+ *
+ * A class used to seed the Countries database table.
+ *
+ * @author Rob Attfield <emailme@robertattfield> <http://www.robertattfield.com>
+ */
+class CountriesTableSeeder extends Seeder
+{
+
+}
